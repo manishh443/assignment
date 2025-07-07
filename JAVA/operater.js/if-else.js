05ec450963b905b5607458ae94e015dc622fd96d
manishh443 <ms0011manishkumar@gmail.com>
2025-07-07 20:41:18 +0530

    task

---
+++ JAVA/operater.js/if-else.js
@@ -0,0 +1,9 @@
+var prompt = require('prompt-sync')();
+let age=prompt('my age is :')
+if (age > 18){
+    console.log('adult')
+
+}
+else{
+    console.log('mijnor')
+}
---
+++ JAVA/operater.js/operator.js
@@ -0,0 +1,7 @@
+//arthamatic =>+,-,/,*,%(modulus),**(exponenential)
+//console.log(10 % 30)
+//assignment operators =>
+    let a=34
+a*=3 //a=a *3
+console.log(a)
+//
